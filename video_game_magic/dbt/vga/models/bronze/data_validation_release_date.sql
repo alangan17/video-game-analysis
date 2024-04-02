796f3164a0d2f1e6e80362a8dd324b8df2329a2d
A N <alangan17@outlook.com>
2024-04-02 20:25:13 +0000

    sqlfluff fix

--- video_game_magic/dbt/vga/models/bronze/data_validation_release_date.sql
+++ video_game_magic/dbt/vga/models/bronze/data_validation_release_date.sql
@@ -1,8 +1,8 @@
-SELECT distinct
-  left(released, 7) as release_date_YYYYMM,
-  count(distinct released) as release_date_count,
-  min(released) as min_release_date,
-  max(released) as max_release_date,
-  count(distinct game_id) as game_count
-FROM {{ source('ext_tbl', 'external_games') }} 
-GROUP BY release_date_YYYYMM
+SELECT DISTINCT
+    left(released, 7) AS release_date_yyyymm,
+    count(DISTINCT released) AS release_date_count,
+    min(released) AS min_release_date,
+    max(released) AS max_release_date,
+    count(DISTINCT game_id) AS game_count
+FROM {{ source('ext_tbl', 'external_games') }}
+GROUP BY release_date_yyyymm
