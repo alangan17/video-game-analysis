64a58d93dff9fd9b1b881bd3d84be9d6a6b2da0b
A N <alangan17@outlook.com>
2024-04-04 04:32:39 +0000

    Add gold models

--- video_game_magic/dbt/vga/models/gold/fact_release_metacritic.sql
+++
@@ -1,4 +0,0 @@
-select *
-from {{ ref("fact_release_by_yyyymm") }}
-where 1=1
-and metacritic is not null
---
+++ video_game_magic/dbt/vga/models/gold/release_by_metacritic_parent_platform.sql
@@ -0,0 +1,69 @@
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "released",
+      "data_type": "date",
+      "granularity": "month"
+    },
+    cluster_by = "game_id",
+    post_hook=[
+        "{{ log_row_count() }}",
+    ],
+    tags=[
+        "silver",
+        "fact"
+    ]
+) }}
+
+with
+base as (
+    select
+        {{ dbt_utils.star(
+            from=ref('fact_release_by_yyyymm')
+        ) }}
+        from {{ ref('fact_release_by_yyyymm') }}
+    ),
+
+    parent_platform as (
+        select
+        {{ dbt_utils.star(
+            from=ref('dim_parent_platforms')
+        ) }}
+            from {{ ref('dim_parent_platforms') }}
+        ),
+
+        has_metacritic as (
+            select *
+            from base
+            where
+                1 = 1
+                and metacritic is not null
+        ),
+
+        release_join_parent_platform as (
+            select
+                r.*,
+                p.parent_platform_name as platform_name
+            from has_metacritic as r
+            left join parent_platform as p
+                on
+                    1 = 1
+                    and r.game_id = p.game_id
+        ),
+
+        final as (
+            select
+                release_year,
+                release_month,
+                release_year_month,
+                released,
+
+                platform_name,
+
+                metacritic,
+                game_id
+            from release_join_parent_platform
+        )
+
+        select *
+        from final
